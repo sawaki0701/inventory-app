e4f01fd8f1901c71dd12af41b1ee17979dc3bbb0
sawaki <sawakik@tanapism.co.jp>
2026-06-26 18:30:28 +0900

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 // src/lib/prisma.ts
-import type { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
