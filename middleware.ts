36bead03be5df7cead3f93470f8d1228f3f85b05
sawaki <sawakik@tanapism.co.jp>
2026-06-30 12:30:47 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,28 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-
-  console.log("======== Middleware ========");
-  console.log("PATH:", pathname);
-
   const token = req.cookies.get("token")?.value;
 
-  console.log("TOKEN:", token);
+  const { pathname } = req.nextUrl;
 
+  // ログイン不要ページ
   if (
     pathname.startsWith("/_next") ||
     pathname.startsWith("/api/auth") ||
     pathname === "/login"
   ) {
-    console.log("Skip middleware");
     return NextResponse.next();
   }
 
+  // トークンなし
   if (!token) {
-    console.log("❌ tokenなし");
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  try {
-    console.log("JWT_SECRET:", process.env.JWT_SECRET);
-
-    jwt.verify(token, process.env.JWT_SECRET!);
-
-    console.log("✅ verify成功");
-
-    return NextResponse.next();
-  } catch (e) {
-    console.error("❌ verify失敗", e);
-
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  return NextResponse.redirect(new URL("/login", req.url));
 }
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api|.*\\?_rsc).*)"],
+};
